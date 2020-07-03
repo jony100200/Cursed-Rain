b57404fae61f9af468c291004d0f21f0087b95e9
Jony <gamingbir@gmail.com>
2020-07-03 19:16:10 -0400

    some changes to level n such

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] protected AudioClip _clickSFX;
+    [SerializeField] [Range(0, 1)] protected float _clickSFXVolume = 0.6f;
+    [SerializeField] protected float _delayLoadingInSeconds = 2f;
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void LoadGameOver()
+    {
+        StartCoroutine(WaitAndLoad());
+    }
+
+    public void PlayClickSound()
+    {
+        AudioSource.PlayClipAtPoint(_clickSFX, Camera.main.transform.position, _clickSFXVolume);
+    }
+
+    private IEnumerator WaitAndLoad()
+    {
+        yield return new WaitForSeconds(_delayLoadingInSeconds);
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
